0507c487ffd62c02ae907c9767274b33599c8562
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-08-26 16:27:57 +0100

    Udpate

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Index() {
+    return (
+        <div>
+            <div>
+                <div>small</div>
+                <div>Big</div>
+            </div>
+        </div>
+    );
+}
+
+export default Index;
