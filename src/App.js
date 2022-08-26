efdf2dedae71e9232ed2195f0b4bc71b9694ae5d
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-08-26 17:07:46 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
+import Navigator from './components/navigate/navigator';
 
 function App() {
   return (
     <div className="App">
+      <Navigator />
       <Routes>
         {/* <Route path="/" element={Index} /> */}
       </Routes>
---
+++ src/components/navigate/navigator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navigator() {
+    return (
+        <div>
+            workings of the
+        </div>
+    );
+}
+
+export default Navigator;
