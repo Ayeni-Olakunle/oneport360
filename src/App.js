d17f8e8870b9a796a50d4487177295bfa8018ff6
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-08-26 16:09:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>small</div>
+      <div>big</div>
+    </div>
+  );
+}
+
+export default App;
