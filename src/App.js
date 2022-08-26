1a8323dffdf4b6ca0b5bf48a4ca48ff753bfd7e1
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-08-26 18:25:08 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
+import appStyle from "./App.module.scss"
 import { Routes, Route } from "react-router-dom";
 import Navigator from './components/navigate/navigator';
+import Dashbord from './components/mainContent/dashbord';
 
 function App() {
   return (
-    <div className="App">
-      <Navigator />
-      <Routes>
-        {/* <Route path="/" element={Index} /> */}
-      </Routes>
+    <div className={appStyle.holdAll}>
+      <div className={appStyle.smallBox}>
+        <Navigator />
+      </div>
+      <div className={appStyle.bigBox}>
+        <Routes>
+          <Route path="/workings" element={<Dashbord />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/navigate/navigator.js
+++ src/components/navigate/navigator.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import Icon from "../../image/Icon.png"
+import person from "../../image/person.png"
+import manyPerson from "../../image/manyperson.png"
+import bus from "../../image/bus.png"
+import tracking from "../../image/tracking.png"
+import document from "../../image/document.png"
+import copy from "../../image/copy.png"
+import invoice from "../../image/invoice.png"
+import logo from "../../image/logo.png"
+import naviStyle from "./navigator.module.scss"
 
 function Navigator() {
     return (
         <div>
-            workings of the
+            <div className={naviStyle.holdLogo}>
+                <img src={logo} alt="logo" />
+            </div>
+            <div>
+                <div>
+                    <span><img src={Icon} alt="Box" /></span>
+                    <span>Dashboard</span>
+                </div>
+                <div>
+                    <span><img src={person} alt="Box" /></span>
+                    <span>Admin</span>
+                </div>
+                <div>
+                    <span><img src={manyPerson} alt="Box" /></span>
+                    <span>Customers</span>
+                </div>
+                <div>
+                    <span><img src={bus} alt="Box" /></span>
+                    <span>Shipments</span>
+                </div>
+                <div>
+                    <span><img src={tracking} alt="Bus" /></span>
+                    <span>Tracking</span>
+                </div>
+                <div>
+                    <span><img src={document} alt="Bus" /></span>
+                    <span>Documents</span>
+                </div>
+                <div>
+                    <span><img src={document} alt="Bus" /></span>
+                    <span>Rates</span>
+                </div>
+                <div>
+                    <span><img src={copy} alt="Bus" /></span>
+                    <span>Quotes</span>
+                </div>
+                <div>
+                    <span><img src={invoice} alt="Bus" /></span>
+                    <span>Invoices</span>
+                </div>
+            </div>
         </div>
     );
 }
